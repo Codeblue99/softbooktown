e05c721e231eb83abc1a67a4a678336477b2f47c
Shayan Ahmed <41948905+Codeblue99@users.noreply.github.com>
2022-10-01 00:42:04 +0500

    Add files via upload

---
+++ function/function.php
@@ -0,0 +1,6 @@
+<?php
+ //$con=mysqli_connect("localhost","root","","db_softbooktown");
+$con=mysqli_connect("localhost","softbook_softbooktown","SoftBookTown123@","softbook_softbooktown");
+// $con=mysqli_connect("localhost","root","","db_booktown");
+?>
+
